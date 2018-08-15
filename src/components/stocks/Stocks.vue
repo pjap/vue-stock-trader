0d69293070959333703634a17571bc0085691166
pjap <pranajaya.pratama@gmail.com>
2018-08-15 22:50:04 +0700

    Create A Stock Page and Use Simple Data

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,9 +1,26 @@
 <template>
-  <h1>The Stocks Components</h1>
+  <div>
+    <appStock v-for="stock in stocks" :stock="stock"></appStock>
+  </div>
 </template>
 
 <script>
+import Stock from './Stock.vue'
+
 export default {
+  data() {
+    return {
+      stocks: [
+        { id: 1, name: 'BMW', price: 110 },
+        { id: 2, name: 'Google', price: 200 },
+        { id: 3, name: 'APPLE', price: 250 },
+        { id: 4, name: 'Twitter', price: 50 }
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
 }
 </script>
 
