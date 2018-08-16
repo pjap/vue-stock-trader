4100ad3f0435d4b8942ff5ce74972df2fa144d70
pjap <pranajaya.pratama@gmail.com>
2018-08-17 00:38:14 +0700

    Create A Random Number In Vuex and Implement TO End Day Feature

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -19,7 +19,10 @@ const mutations = {
     state.stocks = database
   },
   'RND_STOCKS' (state) {
-
+    console.log(JSON.stringify(state.stocks) + 'INI STOCKKKKSSS');
+    state.stocks.forEach(stock => {
+      stock.hargaBarang = Math.round(stock.hargaBarang * (1 + Math.random() - 0.5))
+    })
   }
 }
 
